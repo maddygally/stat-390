bd497f421c26198e2fcea1b72854664c10b75a6f
maddygally <madelyngallagher2025@u.northwestern.edu>
2024-10-03 14:57:42 -0500

    qmd & data cleaning

--- presentation 1/data-cleaning.R
+++ presentation 1/data-cleaning.R
@@ -1,1 +1,13 @@
 ### DATA CLEANING
+
+library(readxl)
+library(tidymodels)
+library(skimr)
+library(janitor)
+
+
+combined_CMIL_scoring_V5 <- read_excel("presentation 1/data/combined CMIL scoring V5.xlsx") %>% 
+  clean_names %>% 
+  slice(-1)
+
+skim(combined_CMIL_scoring_V5)
